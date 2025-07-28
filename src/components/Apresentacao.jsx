6d73c07e796d6070d3be1a5799ead5b5794f3158
Mariana <mari161107@gmail.com>
2025-07-28 15:09:43 +0000

    Passando componentes: Navegação e apresentação

---
+++ src/components/Apresentacao.jsx
@@ -0,0 +1,26 @@
+export default function Apresentacao(){
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[url(./public/FundoAzul.png)] bg-cover bg-center">
+        <header className="text-center mb-8 lg:mt-4.5 scroll-p-[0.5]">
+          <h1 className="text-4xl font-bold text-amber-50 lg:mt-1 text-3.75">Bem vindo/a á</h1>
+          <h1 className="text-4xl font-bold text-amber-50 lg:mt-3 text-3.75">FelisCanis Care</h1>
+        </header>
+  
+        <section className="flex flex-col items-center justify-center">
+          
+          <iframe className="aspect-video w-full max-w-md" src="./public/TCCbeta1.mp4"></iframe>
+  
+          <div className="container text-center mt-4">
+            <p className="text-lg text-amber-50">Um novo olhar tecnológico no mercado pet</p>
+          </div>
+  
+          <button className="mt-8 px-8 py-3 bg-amber-50 text-gray-800 font-bold rounded-full shadow-lg hover:bg-gray-800 hover:text-amber-50 transition duration-300">Saiba mais</button>
+  
+          <div className="footer flex place-items-start item-start mt-8">
+            <i><span className="material-symbols-outlined">download</span></i>
+            <a href=""><p className="ml-2 text-amber-50">Instale o aplicativo</p></a>
+          </div>
+        </section>
+      </div>
+    )
+}
---
+++ src/components/Navegacao/BarraNavegacao.jsx
@@ -0,0 +1,32 @@
+export default function BarraNavegacao() {
+
+    return(
+        <div>
+            <header>
+
+                <button className=""></button>
+
+                <nav className="">
+                        <button></button>
+            
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                </nav>
+
+                <nav className="">
+
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                </nav>
+
+            </header>
+
+        </div>
+    )
+}
