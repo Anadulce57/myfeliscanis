07bfba2c16d167307057c11da01bd8bd36c9d13c
Mariana <mari161107@gmail.com>
2025-10-21 01:28:35 +0000

    atualizações das instruções

--- src/components/Instrucoes/Instrucoes.jsx
+++ src/components/Instrucoes/Instrucoes.jsx
@@ -10,17 +10,16 @@ export default function Instrucoes() {
         <aside className="flex-1">
           <h2 className="text-2xl text-off-brown text-gray-850 font-semibold mb-6 mt-7 dark:text-off-white">Como usar o dispositivo</h2>
           <ol className="space-y-4 list-decimal list-inside text-lg leading-relaxed text-black dark:text-white items-end mt-21">
-            <li>Instale o dispositivo na incubadora e conecte à energia.</li>
+            <li>Instale o dispositivo na parede pelo material de trás.</li>
             <li>
-              Configure pelo app ou painel: defina o tipo de animal e a temperatura ideal.
-              Monitore em tempo real a temperatura do ambiente.
+            Configure pelo app/painel: Faça a ficha de seu bebê.
             </li>
-            <li>Receba alertas se a temperatura sair do ideal.</li>
+            <li>Monitore em tempo real a temperatura do ambiente, qualidade do ar e liberação de gás.</li>
             <li>
-              Controle automático: ativa aquecimento ou ventilação conforme necessário.
+            Controle automático: Alerte o animal nos locais em que ele não deve ficar.
             </li>
-            <li>Acesse o histórico de dados e relatórios.</li>
-            <li>Faça manutenção regular: limpeza e verificação da energia.</li>
+            <li>Acesse o histórico de dados do pet.</li>
+            <li>-Faça manutenção regular: Nos envie mensagem pelo nosso Instagram que liberamos um e-mail para você.</li>
           </ol>
         </aside>
 
